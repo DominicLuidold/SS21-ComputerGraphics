811d24ed94a8a2a9f482ea7d445c4f49492e7cc5
Michael Vogel <mvo8038@students.fhv.at>
2021-04-18 15:45:01 +0200

    init

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    private int currentCount = 0;
+    public TextMeshProUGUI counterField;
+    public Button decreaseButton;
+
+    public void Start()
+    {
+        updateCounter();
+    }
+
+    public void Increase()
+    {
+        currentCount++;
+        updateCounter();
+    }
+
+    public void Decrease()
+    {
+        if(currentCount > 0)
+        {
+            currentCount--;
+            updateCounter();
+        } 
+    }
+    
+    public void ResetCounter()
+    {
+        currentCount = 0;
+        updateCounter();
+    }
+
+    private void updateCounter()
+    {
+        counterField.SetText(currentCount.ToString());
+        if(currentCount <= 0)
+        {
+            decreaseButton.interactable = false;
+        } else
+        {
+            decreaseButton.interactable = true;
+        }
+    }
+}
---
+++ Assets/Scripts/HeartBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartBehaviour : MonoBehaviour
+{
+    public Animator animator;
+    private bool heartBeating = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            heartBeating = !heartBeating;
+            animator.SetBool("heartBeating", heartBeating);
+        } 
+        else if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            animator.ResetTrigger("playHeartBeat2");
+            animator.SetTrigger("playHeartBeat1");
+        } 
+        else if(Input.GetKeyDown(KeyCode.Keypad2)) 
+        {
+            animator.ResetTrigger("playHeartBeat1");
+            animator.SetTrigger("playHeartBeat2");
+        }
+    }
+
+    public void SwitchHeartState()
+    {
+        heartBeating = !heartBeating;
+        animator.SetBool("heartBeating", heartBeating);
+    }
+
+    public void SetHeartState(bool isBeating)
+    {
+        heartBeating = isBeating;
+        animator.SetBool("heartBeating", heartBeating);
+    }
+
+    public void RotateLeft()
+    {
+        transform.Rotate(Vector3.up);
+    }
+
+    public void RotateRight()
+    {
+        transform.Rotate(Vector3.down);
+    }
+
+    public void RotateUp()
+    {
+        transform.Rotate(Vector3.right);
+    }
+
+    public void RotateDown()
+    {
+        transform.Rotate(Vector3.left);
+    }
+}
