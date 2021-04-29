e9a1592df2b2f4abd745ebebc81bdd4f3109ab32
Michael Vogel <mvo8038@students.fhv.at>
2021-04-30 00:44:16 +0200

    Emulator for unity-editor in VrGrabber/Scripts

---
+++ Assets/VrGrabber/Scripts/CustomControllerEmulator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace VrGrabber
+{
+    //class is same as VrgOculusTouchDevice but for unity editor using button A for Grab, S for pulling grabbed object and D for pushing grabbed object
+    public class CustomControllerEmulator : IDevice
+    {
+        private OVRInput.Controller GetOVRController(ControllerSide side)
+        {
+            return (side == ControllerSide.Left) ?
+                OVRInput.Controller.LTouch :
+                OVRInput.Controller.RTouch;
+        }
+
+        public Vector3 GetLocalPosition(ControllerSide side)
+        {
+            return OVRInput.GetLocalControllerPosition(GetOVRController(side));
+        }
+
+        public Quaternion GetLocalRotation(ControllerSide side)
+        {
+            return OVRInput.GetLocalControllerRotation(GetOVRController(side));
+        }
+
+        public bool GetHold(ControllerSide side)
+        {
+            return Input.GetKey(KeyCode.A);
+        }
+
+        public bool GetRelease(ControllerSide side)
+        {
+            return Input.GetKeyUp(KeyCode.A);
+        }
+
+        public bool GetHover(ControllerSide side)
+        {
+            return Input.GetKeyUp(KeyCode.A);
+        }
+
+        public bool GetClick(ControllerSide side)
+        {
+            return Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+        }
+
+        public Vector2 GetCoord(ControllerSide side)
+        {
+            if(Input.GetKey(KeyCode.S))
+            {
+                return new Vector2(0f, -1.0f);
+            } else if(Input.GetKey(KeyCode.D))
+            {
+                return new Vector2(0f, 1.0f);
+            }
+            return new Vector2(0,0);
+        }
+    }
+}
+
---
+++ Assets/VrGrabber/Scripts/VrgDevice.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace VrGrabber
+{
+    public class Device {
+        static IDevice _instance;
+        public static IDevice instance {
+            get {
+                if (_instance == null) {
+#if !UNITY_WSA
+                    _instance = new VrgOculusTouchDevice();
+                    Debug.Log("not wsa");
+#elif UNITY_WSA
+                    _instance = new VrgWinMRMotionControllerDevice();
+                    Debug.Log("wsa");
+#else
+#error "Not implemented."
+#endif
+
+#if UNITY_EDITOR
+                    _instance = new CustomControllerEmulator();
+                    Debug.Log("editor");
+#endif
+                }
+                return _instance;
+            }
+        }
+
+        public Vector3 GetLocalPosition(ControllerSide side) {
+            return _instance.GetLocalPosition(side);
+        }
+
+        public Quaternion GetLocalRotation(ControllerSide side) {
+            return _instance.GetLocalRotation(side);
+        }
+
+        public bool GetHold(ControllerSide side) {
+            return _instance.GetHold(side);
+        }
+
+        public bool GetRelease(ControllerSide side)
+        {
+            return _instance.GetRelease(side);
+        }
+
+        public bool GetHover(ControllerSide side) {
+            return _instance.GetHover(side);
+        }
+
+        public bool GetClick(ControllerSide side) {
+            return _instance.GetClick(side);
+        }
+
+        public Vector2 GetCoord(ControllerSide side) {
+            return _instance.GetCoord(side);
+        }
+    }
+}
